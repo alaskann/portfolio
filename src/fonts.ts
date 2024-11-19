78355341a25ea96fe27f32e935cdc2a351c47200
alaskann <derproka@gmail.com>
2024-11-18 19:52:03 -0600

    Initial commit

---
+++ src/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, VT323 } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const vt323 = VT323({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-vt323",
+});
